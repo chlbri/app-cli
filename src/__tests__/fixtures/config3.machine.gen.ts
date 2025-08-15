37ed748a6a0ab0520bb60ebda4fe12fd68fff742
chlbri <51156116+chlbri@users.noreply.github.com>
2025-08-15 00:55:34 +0000

    refactor: add support for *.gen.ts files in prettier and eslint configurations

--- src/__tests__/fixtures/config3.machine.gen.ts
+++ src/__tests__/fixtures/config3.machine.gen.ts
@@ -1,275 +1,258 @@
+/**
+ * This file is auto-generated. Do not edit manually.
+ */
 
-  /**
-   * This file is auto-generated. Do not edit manually.
-   */
-
-  export const __tsSchemas = {
-  "config3": {
-    "initial": [
-      "idle",
-      "authenticating",
-      "authenticated",
-      "loggedOut"
+export const __tsSchemas = {
+  config3: {
+    initial: ['idle', 'authenticating', 'authenticated', 'loggedOut'],
+    targets: [
+      '/idle',
+      '/authenticating',
+      '/authenticating/credentials',
+      '/authenticating/credentials/gathering',
+      '/authenticating/credentials/validating',
+      '/authenticating/credentials/validating/pending',
+      '/authenticating/credentials/validating/success',
+      '/authenticating/credentials/validating/failure',
+      '/authenticating/session',
+      '/authenticated',
+      '/authenticated/active',
+      '/authenticated/idle',
+      '/loggedOut',
     ],
-    "targets": [
-      "/idle",
-      "/authenticating",
-      "/authenticating/credentials",
-      "/authenticating/credentials/gathering",
-      "/authenticating/credentials/validating",
-      "/authenticating/credentials/validating/pending",
-      "/authenticating/credentials/validating/success",
-      "/authenticating/credentials/validating/failure",
-      "/authenticating/session",
-      "/authenticated",
-      "/authenticated/active",
-      "/authenticated/idle",
-      "/loggedOut"
-    ],
-    "states": {
-      "idle": {
-        "targets": [
-          "/",
-          "/authenticating",
-          "/authenticating/credentials",
-          "/authenticating/credentials/gathering",
-          "/authenticating/credentials/validating",
-          "/authenticating/credentials/validating/pending",
-          "/authenticating/credentials/validating/success",
-          "/authenticating/credentials/validating/failure",
-          "/authenticating/session",
-          "/authenticated",
-          "/authenticated/active",
-          "/authenticated/idle",
-          "/loggedOut"
-        ]
-      },
-      "authenticating": {
-        "targets": [
-          "/",
-          "/idle",
-          "/authenticating/credentials",
-          "/authenticating/credentials/gathering",
-          "/authenticating/credentials/validating",
-          "/authenticating/credentials/validating/pending",
-          "/authenticating/credentials/validating/success",
-          "/authenticating/credentials/validating/failure",
-          "/authenticating/session",
-          "/authenticated",
-          "/authenticated/active",
-          "/authenticated/idle",
-          "/loggedOut"
+    states: {
+      idle: {
+        targets: [
+          '/',
+          '/authenticating',
+          '/authenticating/credentials',
+          '/authenticating/credentials/gathering',
+          '/authenticating/credentials/validating',
+          '/authenticating/credentials/validating/pending',
+          '/authenticating/credentials/validating/success',
+          '/authenticating/credentials/validating/failure',
+          '/authenticating/session',
+          '/authenticated',
+          '/authenticated/active',
+          '/authenticated/idle',
+          '/loggedOut',
         ],
-        "states": {
-          "credentials": {
-            "initial": [
-              "gathering",
-              "validating"
+      },
+      authenticating: {
+        targets: [
+          '/',
+          '/idle',
+          '/authenticating/credentials',
+          '/authenticating/credentials/gathering',
+          '/authenticating/credentials/validating',
+          '/authenticating/credentials/validating/pending',
+          '/authenticating/credentials/validating/success',
+          '/authenticating/credentials/validating/failure',
+          '/authenticating/session',
+          '/authenticated',
+          '/authenticated/active',
+          '/authenticated/idle',
+          '/loggedOut',
+        ],
+        states: {
+          credentials: {
+            initial: ['gathering', 'validating'],
+            targets: [
+              '/',
+              '/idle',
+              '/authenticating',
+              '/authenticating/credentials/gathering',
+              '/authenticating/credentials/validating',
+              '/authenticating/credentials/validating/pending',
+              '/authenticating/credentials/validating/success',
+              '/authenticating/credentials/validating/failure',
+              '/authenticating/session',
+              '/authenticated',
+              '/authenticated/active',
+              '/authenticated/idle',
+              '/loggedOut',
             ],
-            "targets": [
-              "/",
-              "/idle",
-              "/authenticating",
-              "/authenticating/credentials/gathering",
-              "/authenticating/credentials/validating",
-              "/authenticating/credentials/validating/pending",
-              "/authenticating/credentials/validating/success",
-              "/authenticating/credentials/validating/failure",
-              "/authenticating/session",
-              "/authenticated",
-              "/authenticated/active",
-              "/authenticated/idle",
-              "/loggedOut"
-            ],
-            "states": {
-              "gathering": {
-                "targets": [
-                  "/",
-                  "/idle",
-                  "/authenticating",
-                  "/authenticating/credentials",
-                  "/authenticating/credentials/validating",
-                  "/authenticating/credentials/validating/pending",
-                  "/authenticating/credentials/validating/success",
-                  "/authenticating/credentials/validating/failure",
-                  "/authenticating/session",
-                  "/authenticated",
-                  "/authenticated/active",
-                  "/authenticated/idle",
-                  "/loggedOut"
-                ]
+            states: {
+              gathering: {
+                targets: [
+                  '/',
+                  '/idle',
+                  '/authenticating',
+                  '/authenticating/credentials',
+                  '/authenticating/credentials/validating',
+                  '/authenticating/credentials/validating/pending',
+                  '/authenticating/credentials/validating/success',
+                  '/authenticating/credentials/validating/failure',
+                  '/authenticating/session',
+                  '/authenticated',
+                  '/authenticated/active',
+                  '/authenticated/idle',
+                  '/loggedOut',
+                ],
               },
-              "validating": {
-                "initial": [
-                  "pending",
-                  "success",
-                  "failure"
+              validating: {
+                initial: ['pending', 'success', 'failure'],
+                targets: [
+                  '/',
+                  '/idle',
+                  '/authenticating',
+                  '/authenticating/credentials',
+                  '/authenticating/credentials/gathering',
+                  '/authenticating/credentials/validating/pending',
+                  '/authenticating/credentials/validating/success',
+                  '/authenticating/credentials/validating/failure',
+                  '/authenticating/session',
+                  '/authenticated',
+                  '/authenticated/active',
+                  '/authenticated/idle',
+                  '/loggedOut',
                 ],
-                "targets": [
-                  "/",
-                  "/idle",
-                  "/authenticating",
-                  "/authenticating/credentials",
-                  "/authenticating/credentials/gathering",
-                  "/authenticating/credentials/validating/pending",
-                  "/authenticating/credentials/validating/success",
-                  "/authenticating/credentials/validating/failure",
-                  "/authenticating/session",
-                  "/authenticated",
-                  "/authenticated/active",
-                  "/authenticated/idle",
-                  "/loggedOut"
-                ],
-                "states": {
-                  "pending": {
-                    "targets": [
-                      "/",
-                      "/idle",
-                      "/authenticating",
-                      "/authenticating/credentials",
-                      "/authenticating/credentials/gathering",
-                      "/authenticating/credentials/validating",
-                      "/authenticating/credentials/validating/success",
-                      "/authenticating/credentials/validating/failure",
-                      "/authenticating/session",
-                      "/authenticated",
-                      "/authenticated/active",
-                      "/authenticated/idle",
-                      "/loggedOut"
-                    ]
+                states: {
+                  pending: {
+                    targets: [
+                      '/',
+                      '/idle',
+                      '/authenticating',
+                      '/authenticating/credentials',
+                      '/authenticating/credentials/gathering',
+                      '/authenticating/credentials/validating',
+                      '/authenticating/credentials/validating/success',
+                      '/authenticating/credentials/validating/failure',
+                      '/authenticating/session',
+                      '/authenticated',
+                      '/authenticated/active',
+                      '/authenticated/idle',
+                      '/loggedOut',
+                    ],
                   },
-                  "success": {
-                    "targets": [
-                      "/",
-                      "/idle",
-                      "/authenticating",
-                      "/authenticating/credentials",
-                      "/authenticating/credentials/gathering",
-                      "/authenticating/credentials/validating",
-                      "/authenticating/credentials/validating/pending",
-                      "/authenticating/credentials/validating/failure",
-                      "/authenticating/session",
-                      "/authenticated",
-                      "/authenticated/active",
-                      "/authenticated/idle",
-                      "/loggedOut"
-                    ]
+                  success: {
+                    targets: [
+                      '/',
+                      '/idle',
+                      '/authenticating',
+                      '/authenticating/credentials',
+                      '/authenticating/credentials/gathering',
+                      '/authenticating/credentials/validating',
+                      '/authenticating/credentials/validating/pending',
+                      '/authenticating/credentials/validating/failure',
+                      '/authenticating/session',
+                      '/authenticated',
+                      '/authenticated/active',
+                      '/authenticated/idle',
+                      '/loggedOut',
+                    ],
                   },
-                  "failure": {
-                    "targets": [
-                      "/",
-                      "/idle",
-                      "/authenticating",
-                      "/authenticating/credentials",
-                      "/authenticating/credentials/gathering",
-                      "/authenticating/credentials/validating",
-                      "/authenticating/credentials/validating/pending",
-                      "/authenticating/credentials/validating/success",
-                      "/authenticating/session",
-                      "/authenticated",
-                      "/authenticated/active",
-                      "/authenticated/idle",
-                      "/loggedOut"
-                    ]
-                  }
-                }
-              }
-            }
+                  failure: {
+                    targets: [
+                      '/',
+                      '/idle',
+                      '/authenticating',
+                      '/authenticating/credentials',
+                      '/authenticating/credentials/gathering',
+                      '/authenticating/credentials/validating',
+                      '/authenticating/credentials/validating/pending',
+                      '/authenticating/credentials/validating/success',
+                      '/authenticating/session',
+                      '/authenticated',
+                      '/authenticated/active',
+                      '/authenticated/idle',
+                      '/loggedOut',
+                    ],
+                  },
+                },
+              },
+            },
           },
-          "session": {
-            "targets": [
-              "/",
-              "/idle",
-              "/authenticating",
-              "/authenticating/credentials",
-              "/authenticating/credentials/gathering",
-              "/authenticating/credentials/validating",
-              "/authenticating/credentials/validating/pending",
-              "/authenticating/credentials/validating/success",
-              "/authenticating/credentials/validating/failure",
-              "/authenticated",
-              "/authenticated/active",
-              "/authenticated/idle",
-              "/loggedOut"
-            ]
-          }
-        }
-      },
-      "authenticated": {
-        "initial": [
-          "active",
-          "idle"
-        ],
-        "targets": [
-          "/",
-          "/idle",
-          "/authenticating",
-          "/authenticating/credentials",
-          "/authenticating/credentials/gathering",
-          "/authenticating/credentials/validating",
-          "/authenticating/credentials/validating/pending",
-          "/authenticating/credentials/validating/success",
-          "/authenticating/credentials/validating/failure",
-          "/authenticating/session",
-          "/authenticated/active",
-          "/authenticated/idle",
-          "/loggedOut"
-        ],
-        "states": {
-          "active": {
-            "targets": [
-              "/",
-              "/idle",
-              "/authenticating",
-              "/authenticating/credentials",
-              "/authenticating/credentials/gathering",
-              "/authenticating/credentials/validating",
-              "/authenticating/credentials/validating/pending",
-              "/authenticating/credentials/validating/success",
-              "/authenticating/credentials/validating/failure",
-              "/authenticating/session",
-              "/authenticated",
-              "/authenticated/idle",
-              "/loggedOut"
-            ]
+          session: {
+            targets: [
+              '/',
+              '/idle',
+              '/authenticating',
+              '/authenticating/credentials',
+              '/authenticating/credentials/gathering',
+              '/authenticating/credentials/validating',
+              '/authenticating/credentials/validating/pending',
+              '/authenticating/credentials/validating/success',
+              '/authenticating/credentials/validating/failure',
+              '/authenticated',
+              '/authenticated/active',
+              '/authenticated/idle',
+              '/loggedOut',
+            ],
           },
-          "idle": {
-            "targets": [
-              "/",
-              "/idle",
-              "/authenticating",
-              "/authenticating/credentials",
-              "/authenticating/credentials/gathering",
-              "/authenticating/credentials/validating",
-              "/authenticating/credentials/validating/pending",
-              "/authenticating/credentials/validating/success",
-              "/authenticating/credentials/validating/failure",
-              "/authenticating/session",
-              "/authenticated",
-              "/authenticated/active",
-              "/loggedOut"
-            ]
-          }
-        }
+        },
       },
-      "loggedOut": {
-        "targets": [
-          "/",
-          "/idle",
-          "/authenticating",
-          "/authenticating/credentials",
-          "/authenticating/credentials/gathering",
-          "/authenticating/credentials/validating",
-          "/authenticating/credentials/validating/pending",
-          "/authenticating/credentials/validating/success",
-          "/authenticating/credentials/validating/failure",
-          "/authenticating/session",
-          "/authenticated",
-          "/authenticated/active",
-          "/authenticated/idle"
-        ]
-      }
-    }
-  }
+      authenticated: {
+        initial: ['active', 'idle'],
+        targets: [
+          '/',
+          '/idle',
+          '/authenticating',
+          '/authenticating/credentials',
+          '/authenticating/credentials/gathering',
+          '/authenticating/credentials/validating',
+          '/authenticating/credentials/validating/pending',
+          '/authenticating/credentials/validating/success',
+          '/authenticating/credentials/validating/failure',
+          '/authenticating/session',
+          '/authenticated/active',
+          '/authenticated/idle',
+          '/loggedOut',
+        ],
+        states: {
+          active: {
+            targets: [
+              '/',
+              '/idle',
+              '/authenticating',
+              '/authenticating/credentials',
+              '/authenticating/credentials/gathering',
+              '/authenticating/credentials/validating',
+              '/authenticating/credentials/validating/pending',
+              '/authenticating/credentials/validating/success',
+              '/authenticating/credentials/validating/failure',
+              '/authenticating/session',
+              '/authenticated',
+              '/authenticated/idle',
+              '/loggedOut',
+            ],
+          },
+          idle: {
+            targets: [
+              '/',
+              '/idle',
+              '/authenticating',
+              '/authenticating/credentials',
+              '/authenticating/credentials/gathering',
+              '/authenticating/credentials/validating',
+              '/authenticating/credentials/validating/pending',
+              '/authenticating/credentials/validating/success',
+              '/authenticating/credentials/validating/failure',
+              '/authenticating/session',
+              '/authenticated',
+              '/authenticated/active',
+              '/loggedOut',
+            ],
+          },
+        },
+      },
+      loggedOut: {
+        targets: [
+          '/',
+          '/idle',
+          '/authenticating',
+          '/authenticating/credentials',
+          '/authenticating/credentials/gathering',
+          '/authenticating/credentials/validating',
+          '/authenticating/credentials/validating/pending',
+          '/authenticating/credentials/validating/success',
+          '/authenticating/credentials/validating/failure',
+          '/authenticating/session',
+          '/authenticated',
+          '/authenticated/active',
+          '/authenticated/idle',
+        ],
+      },
+    },
+  },
 } as const;
-  
